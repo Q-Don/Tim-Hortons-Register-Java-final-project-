eea31cdc1ad26db7be294b35f96e14dd4a678bde
Quardin <>
2017-07-25 17:08:59 -0400

    Tim Hortons intial

---
+++ src/timhortonsregisterjavafinalproject/RegisterController.java
@@ -0,0 +1,39 @@
+/*
+Simarpreet Kaur, Quardin Lyttle
+Object Oriented Program 2 Java- PROG 24178 - P11 12248 / 106
+Computer Engineering Tech
+Sheridan College
+ */
+package timhortonsregisterjavafinalproject;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Simarpreet Kaur, Quardin Lyttle
+ */
+public class RegisterController implements Initializable
+{
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event)
+    {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        // TODO
+    }    
+    
+}
---
+++ src/timhortonsregisterjavafinalproject/TimHortonsRegisterJavaFinalProject.java
@@ -0,0 +1,41 @@
+/*
+Simarpreet Kaur, Quardin Lyttle
+Object Oriented Program 2 Java- PROG 24178 - P11 12248 / 106
+Computer Engineering Tech
+Sheridan College
+ */
+package timhortonsregisterjavafinalproject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Simarpreet Kaur, Quardin Lyttle
+ */
+public class TimHortonsRegisterJavaFinalProject extends Application
+{
+    
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("Register.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+    
+}
