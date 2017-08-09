9395fb8d2c862900fda6246f5d3fef0bdbd117a7
Quardin <peel@10.48.10.201>
2017-08-08 21:19:08 -0400

    made small changes to newcard

--- src/timhortonsregisterjavafinalproject/MenuController.java
+++ src/timhortonsregisterjavafinalproject/MenuController.java
@@ -13,6 +13,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 
 /**
@@ -24,6 +25,11 @@ public class MenuController implements Initializable
 
      @FXML
      private Button back;
+     @FXML
+     private Button cancel;
+     @FXML
+     private Button total;
+     
      @FXML
      private Button french;
      @FXML
@@ -50,6 +56,8 @@ public class MenuController implements Initializable
      private Button smoothie;
      @FXML
      private TextArea screen;
+     @FXML
+     private ComboBox<String> payment;
      
      ArrayList<String> order = new ArrayList<>();
      ArrayList<Double> cost = new ArrayList<>();
@@ -68,22 +76,146 @@ public class MenuController implements Initializable
 
 
      }
+     
+     @FXML
+     private void Total(ActionEvent event)
+     {
+         double total = 0;
+         for(int i=0; i<cost.size();i++)
+         {
+             total = cost.get(i)+total;
+         }
+         screen.appendText("--------------------------------\n");
+         screen.appendText("\t\t\t\t"+total);
+     }
+     
      @FXML
      private void French(ActionEvent event)
      {
          double cost =1.30;
          String name = "French Vanilla";
-         screen.appendText(name+"\t\t\t"+cost);
+         screen.appendText(name+"\t\t\t"+cost+"\n");
+         OrderList(name,cost);
+         
+     }
+     
+     @FXML
+     private void Capp (ActionEvent event)
+     {
+         double cost =1.60;
+         String name = "Cappauccino";
+         screen.appendText(name+"\t\t\t"+cost+"\n");
+         OrderList(name,cost);
+         
+     }
+     
+     @FXML
+     private void Latte(ActionEvent event)
+     {
+         double cost =1.45;
+         String name = "Latte";
+         screen.appendText(name+"\t\t\t\t"+cost+"\n");
+         OrderList(name,cost);
+         
+     }
+     
+     @FXML
+     private void HotChocolate(ActionEvent event)
+     {
+         double cost =1.50;
+         String name = "Hot Chocolate";
+         screen.appendText(name+"\t\t\t"+cost+"\n");
+         OrderList(name,cost);
+         
+     }
+     
+     @FXML
+     private void Original(ActionEvent event)
+     {
+         double cost =1.00;
+         String name = "Orignal Blend";
+         screen.appendText(name+"\t\t\t"+cost+"\n");
+         OrderList(name,cost);
+         
+     }
+     
+     @FXML
+     private void DarkRoast(ActionEvent event)
+     {
+         double cost =1.10;
+         String name = "Dark Roast";
+         screen.appendText(name+"\t\t\t"+cost+"\n");
+         OrderList(name,cost);
+         
+     }
+     
+     @FXML
+     private void Decaf(ActionEvent event)
+     {
+         double cost =1.15;
+         String name = "Decaf";
+         screen.appendText(name+"\t\t\t\t"+cost+"\n");
+         OrderList(name,cost);
+         
+     }
+     
+     @FXML
+     private void Tea(ActionEvent event)
+     {
+         double cost =1.70;
+         String name = "Tea";
+         screen.appendText(name+"\t\t\t\t"+cost+"\n");
+         OrderList(name,cost);
+         
+     }
+     
+     @FXML
+     private void IcedCapp(ActionEvent event)
+     {
+         double cost =2.10;
+         String name = "Iced Capp";
+         screen.appendText(name+"\t\t\t"+cost+"\n");
+         OrderList(name,cost);
+         
+     }
+     
+     @FXML
+     private void IcedCoffee(ActionEvent event)
+     {
+         double cost =1.90;
+         String name = "Iced Coffee";
+         screen.appendText(name+"\t\t\t"+cost+"\n");
          OrderList(name,cost);
          
      }
      
      
+     @FXML
+     private void Lemonade(ActionEvent event)
+     {
+         double cost =2.45;
+         String name = "Frozen Lemonade";
+         screen.appendText(name+"\t\t\t"+cost+"\n");
+         OrderList(name,cost);
+         
+     }
+     
+     @FXML
+     private void Smoothie(ActionEvent event)
+     {
+         double cost =2.45;
+         String name = "Fruit Smoothie";
+         screen.appendText(name+"\t\t\t"+cost+"\n");
+         OrderList(name,cost);
+         
+     }
  
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
+        screen.setText("Item \t\t\t\t Cost");
+        screen.setEditable(false);
+        
     }    
     
 }
